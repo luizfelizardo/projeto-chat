683a2b584bee3382337adfecdbaf4a3d4fd02ad7
luiz <luiz.fatec2008@gmail.com>
2025-03-09 10:49:12 -0300

    local Storage incluido no código

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,118 +1,3 @@
-$(document).ready(function(){
-    // set #background-* to full window height and fade in the body
-    var width = $(window).width();
-    var height = $(window).height();
-        $('#background-container, #background-1, #background-2').css({
-            'min-width': width,
-            'min-height': height
-        });
-
-    // call new svg and start recreate svg timeout
-    svgNew();
-    recreateSvg();
-});
-
-// set global svg object
-var svg = {};
-// used to determine which background to draw to
-var draw = 1;
-// create new svg 
-var svgNew = function(){
-    svg.t = new Trianglify({
-        noiseIntensity: 0,
-    });
-    // set svg size to window height and width
-    svg.width = $(window).width();
-    svg.height = $(window).height();
-    svg.pattern = svg.t.generate(svg.width, svg.height);
-    // draw svg on to either background 1 or 2
-    if (draw === 1) {
-        svgDraw1();
-    } else {
-        svgDraw2();
-    }
-}; // end svgNew
-
-// draw svg on to bg1 and call fade
-// if called with resize, redraw the svg to match new size and do not call fade
-var svgDraw1 = function (resize){
-    draw = 2;
-    if (resize === 'resize') {  
-        svg.pattern = svg.t.generate(svg.width, svg.height);
-        $('#background-1').css({
-            'min-width': svg.width,
-            'min-height': svg.height,
-            'background': svg.pattern.dataUrl
-        });
-        $('#contact-background-1').css({
-            'min-width': svg.width,
-            'min-height': (svg.height / 2),
-            'background': svg.pattern.dataUrl
-        });
-    } else {
-        $('.background-1').css({
-            'background': svg.pattern.dataUrl
-        });
-        fade1();
-    }
-}; // end svgDraw1
-
-// same as above but for bg2
-var svgDraw2 = function(resize){
-    draw = 1;
-    if (resize === 'resize') {  
-        svg.pattern = svg.t.generate(svg.width, svg.height);
-        $('#background-2').css({
-            'min-width': svg.width,
-            'min-height': svg.height,
-            'background': svg.pattern.dataUrl
-        });
-        $('#contact-background-2').css({
-            'min-width': svg.width,
-            'min-height': (svg.height / 2),
-            'background': svg.pattern.dataUrl
-        });
-    } else {
-        $('.background-2').css({
-            'background': svg.pattern.dataUrl
-        });
-        fade2();
-    }
-}; // end svgDraw2
-
-// fade in bg1 and fade our bg2
-var fade1 = function(){
-    $('.background-1').velocity("fadeIn", { duration: 3000 });
-    $('.background-2').velocity("fadeOut", { duration: 4000 });
-};
-// fade in bg2 and fade out bg1
-var fade2 = function(){
-    $('.background-2').velocity("fadeIn", { duration: 3000 });
-    $('.background-1').velocity("fadeOut", { duration: 4000 });
-};
-
-// timeout function to create new svg every 5 seconds
-var recreateSvg = function(){
-    window.setInterval(svgNew, 5000);
-};
-
-// redraw the current svg to match screen size on resize
-$(window).resize(function() {
-    svg.width = $(window).width();
-    svg.height = $(window).height();
-    $('#background-container').css({
-        'min-width': svg.width,
-        'min-height': svg.height
-    });
-    $('#contact-container').css({
-        'min-width': svg.width,
-        'min-height': (svg.height / 2)
-    });
-    svgDraw1('resize');
-    svgDraw2('resize');
-});
-
-
 // login elements
 const login = document.querySelector(".login");
 const loginForm = login.querySelector(".login__form");
@@ -135,15 +20,14 @@ const colors = [
 ];
 
 const user = { id: "", name: "", color: "" };
+const messages = JSON.parse(localStorage.getItem("chatMessages")) || [];
 
 let websocket;
 
 const createMessageSelfElement = (content) => {
     const div = document.createElement("div");
-
     div.classList.add("message--self", `user-${user.id}`);
     div.innerHTML = content;
-
     return div;
 };
 
@@ -164,12 +48,6 @@ const createMessageOtherElement = (content, sender, senderColor, senderId) => {
     return div;
 };
 
-const createMessageElement = (content) => {
-    const div = document.createElement("div");
-    div.textContent = content;
-    return div;
-};
-
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
@@ -182,6 +60,12 @@ const scrollScreen = () => {
     });
 };
 
+const repaintChatMessages = () => {
+    chatMessages.style.display = 'none';
+    chatMessages.offsetHeight;
+    chatMessages.style.display = 'flex';
+}
+
 const processMessage = ({ data }) => {
     const { userId, userName, userColor, content } = JSON.parse(data);
 
@@ -191,8 +75,10 @@ const processMessage = ({ data }) => {
             : createMessageOtherElement(content, userName, userColor, userId);
 
     chatMessages.appendChild(message);
-
+    messages.push({ userId, userName, userColor, content });
+    localStorage.setItem("chatMessages", JSON.stringify(messages));
     scrollScreen();
+    repaintChatMessages();
 };
 
 const handleLogin = (event) => {
@@ -202,11 +88,26 @@ const handleLogin = (event) => {
     user.name = loginInput.value;
     user.color = getRandomColor();
 
+    localStorage.setItem("userName", user.name);
+
     login.style.display = "none";
     chat.style.display = "flex";
 
     websocket = new WebSocket("wss://projeto-chat-jq7k.onrender.com");
+
+    websocket.onopen = () => {
+        console.log("WebSocket connection opened");
+    };
+
     websocket.onmessage = processMessage;
+
+    websocket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+    };
+
+    websocket.onclose = () => {
+        console.log("WebSocket connection closed");
+    };
 };
 
 const sendMessage = (event) => {
@@ -221,7 +122,6 @@ const sendMessage = (event) => {
         };
 
         websocket.send(JSON.stringify(message));
-
         chatInput.value = "";
     } else {
         console.error("WebSocket connection is not open.");
@@ -234,21 +134,62 @@ const sairChat = () => {
         websocket = null;
         chatMessages.innerHTML = "";
         console.log("Você saiu do chat.");
-
-        // Limpa o estado do usuário
-        user.id = "";
-        user.name = "";
-        user.color = "";
-
-        // Esconde a tela de chat e mostra a tela de login
-        chat.style.display = "none";
-        login.style.display = "flex";
+    } else {
+        console.log("WebSocket não está aberto ou não existe.");
     }
+
+    user.id = "";
+    user.name = "";
+    user.color = "";
+
+    localStorage.removeItem("userName");
+    localStorage.removeItem("chatMessages");
+
+    chat.style.display = "none";
+    login.style.display = "flex";
+    repaintChatMessages();
 };
 
 if (sairChatButton) {
     sairChatButton.addEventListener("click", sairChat);
 }
+
+// Verifica o localStorage apenas após o carregamento da página
+document.addEventListener("DOMContentLoaded", () => {
+    const storedUserName = localStorage.getItem("userName");
+    if (storedUserName) {
+        user.id = crypto.randomUUID();
+        user.name = storedUserName;
+        user.color = getRandomColor();
+        login.style.display = "none";
+        chat.style.display = "flex";
+        websocket = new WebSocket("wss://projeto-chat-jq7k.onrender.com");
+        websocket.onopen = () => {
+            console.log("WebSocket connection opened");
+        };
+        websocket.onmessage = processMessage;
+        websocket.onerror = (error) => {
+            console.error("WebSocket error:", error);
+        };
+        websocket.onclose = () => {
+            console.log("WebSocket connection closed");
+        };
+        messages.forEach((message) => {
+            const messageElement =
+                message.userId == user.id
+                    ? createMessageSelfElement(message.content)
+                    : createMessageOtherElement(
+                          message.content,
+                          message.userName,
+                          message.userColor,
+                          message.userId
+                      );
+            chatMessages.appendChild(messageElement);
+        });
+        scrollScreen();
+        repaintChatMessages();
+    }
+});
 
 loginForm.addEventListener("submit", handleLogin);
 chatForm.addEventListener("submit", sendMessage);
